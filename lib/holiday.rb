2f84c513943d9680f1db254e61340a277c9df467
Matthew Corralejo <mattcorralejo1@gmail.com>
2019-12-20 12:16:01 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -52,6 +52,7 @@ def all_supplies_in_holidays(holiday_hash)
       i.capitalize}.join("")}: #{item.join(",")}"
 end
 end
+end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
