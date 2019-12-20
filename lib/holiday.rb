6fc473a785f7e838c6bb2cdc34c04cb9018b7aeb
Matthew Corralejo <mattcorralejo1@gmail.com>
2019-12-20 12:10:29 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -47,6 +47,7 @@ end
 def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season,holiday|
     puts "#{season.capitilize}:"
+    holiday.each
 end
 
 def all_holidays_with_bbq(holiday_hash)
